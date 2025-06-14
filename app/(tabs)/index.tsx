b8187a1a08202843408abce00a3b2e9db13f200f
snehasingh-25 <sneha251104@gmail.com>
2025-06-14 11:24:59 +0530

    Fetched latest movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,43 @@
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView, ActivityIndicator, FlatList } from "react-native";
 import Link from "expo-router/link";
 import { images } from "@/constants/images";
 import { icons } from '../../constants/icons';
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({
+    query: ""
+  }))
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5">
-          <SearchBar onPress={() => router.push("/search")} placeholder="Search for movie" />
-        </View>
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000fff" className="mt-10 self-center" />
+        ) : moviesError ? (
+          <Text className="text-white">Error:{moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar onPress={() => router.push("/search")} placeholder="Search for movie" />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest  movies</Text>
+              <FlatList
+              data={movies}
+              renderItem={({item})=>(
+                <Text className="text-white mb-2">{item.title}</Text>
+              )}
+              ></FlatList>
+            </>
+          </View>
+        )}
+
       </ScrollView>
     </View>
   );
-}
+};
--- services/api.ts
+++ services/api.ts
@@ -21,4 +21,4 @@ export const fetchMovies = async ({query}:{query:string}) => {
     return data.results;
 }
 
-const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
